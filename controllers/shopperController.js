db27031f56c595a8af53d0f919e35a84bdd1f87d
yash sharma <yashkr00129@gmail.com>
2022-10-17 11:36:32 +0530

    Restaurant search

--- controllers/shopperController.js
+++ controllers/shopperController.js
@@ -1481,21 +1481,24 @@ exports.getRestaurantsInState = [
     }
     try {
       const search = req.body?.search ? req.body.search : "";
+      let restaurants = [];
+      if (search) {
+        restaurants = await Restaurant.find({
+          $or: [
+            { fullName: { $regex: new RegExp(search, "i") } },
+            { email: { $regex: new RegExp(search, "i") } },
+            { phone: { $regex: new RegExp(search, "i") } },
+            { storeName: { $regex: new RegExp(search, "i") } }
+          ],
+        }).populate("address");
+      } else {
+        restaurants = await Restaurant.find().populate("address");
+      }
 
       const user = await Shopper.findOne(
         { _id: req.user._id },
         { address: 1 }
       ).populate("address");
-
-      let restaurants = await Restaurant.find({
-        $or: [
-          { fullName: { $regex: new RegExp(search, "i") } },
-          { email: { $regex: new RegExp(search, "i") } },
-          { phone: { $regex: new RegExp(search, "i") } },
-          { region: user.address.region }
-        ],
-      }).populate("address");
-
       const states = [];
       for (let address of user?.address) {
         states.push(address?.state);
